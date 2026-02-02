cbb9048dcef353f72cb4391e753feea3ade009ca
learningcodeniko <6nicholasp6@gmail.com>
2026-02-02 15:18:50 +0100

    HW - part 1

--- HWSession5.py
+++ HWSession5.py
@@ -11,3 +11,11 @@ except:
     print("I did not see that coming")
 else:
     print("Thanks for playing fair")
+if gross < 1000:
+    print("Net Income =", gross * (0.9+0.01*kids))
+elif gross < 2000:
+    print("Net Income =", gross * (0.88+0.01*kids))
+elif gross < 4000:
+    print("Net Income =", gross * (0.86+0.005*kids))
+else:
+    print("Net Income =", gross * (0.82+0.005*kids))
